c98dc044a2cbc869408892a856e4d513de0db0f4
Tommy Speigner <tspeigner@gmail.com>
2017-04-17 18:13:06 -0700

    updated

--- manifests/myelastic.pp
+++ manifests/myelastic.pp
@@ -1,12 +1,12 @@
 # my elasticsearch example code.
 class myelastic {
   class { 'elasticsearch':
-    ensure       => 'absent',
+    #ensure       => 'absent',
     java_install => true,
     manage_repo  => true,
     repo_version => '5.x',
   }
 
-  elasticsearch::instance { $::fqdn-01: }
+  elasticsearch::instance { {$::fqdn}-01: }
 
 }
