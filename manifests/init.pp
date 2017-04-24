09810cad1dedac87a8de2b0764b611d7cc956471
Tommy Speigner <tspeigner@gmail.com>
2017-04-24 16:26:50 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,1 @@
-# my elasticsearch example code.
-class myelastic {
-  class { 'elasticsearch':
-    java_install => true,
-    manage_repo  => true,
-    repo_version => '5.x',
-  }
-
-  elasticsearch::instance { '::$fqdn-es-01': }
-
-}
+include myelastic
